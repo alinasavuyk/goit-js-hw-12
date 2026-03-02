dfb18d8c158ba235b97618269ae07337fcb29ff5
alinasavuyk <alinabondarenko>
2026-03-02 11:20:43 +0200

    fix3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {getImagesByQuery} from './js/pixabay-api.js'
 import {createGallery, clearGallery, showLoader, hideLoader, showLoadMoreButton, hideLoadMoreButton} from './js/render-functions.js'
-
-   import iziToast from "izitoast";
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 const form =document.querySelector('.form')
 const input =document.querySelector('[name="search-text"]')
@@ -11,7 +10,7 @@ let page = 1;
 let results;
 let query='';
 const per_page=15;
-showLoader()
+
 async function handleSubmit (event){
 event.preventDefault();
 query = input.value.trim();
@@ -31,8 +30,6 @@ try{
     results = await getImagesByQuery(query, page);
     hideLoader()
    if (!results.hits || results.hits.length === 0) {
-    hideLoadMoreButton()
-    showLoader()
                 iziToast.error({
                 message: 'Sorry, there are no images matching your search query. Please try again!',
                 position: 'topRight',
@@ -40,11 +37,16 @@ try{
                 return;
             } 
                 createGallery(results.hits);  
-               
-                
-                if(results.totalHits>per_page){
-                  showLoadMoreButton() 
-                }
+                form.reset();
+
+        const totalPages = Math.ceil(results.totalHits / per_page);
+        if (totalPages > 1) {
+            showLoadMoreButton();
+        } else {
+            iziToast.info({
+                message: "We're sorry, but you've reached the end of search results.",
+                position: 'topRight',
+            });}
 }
 catch(error){
     hideLoader()
@@ -54,14 +56,15 @@ catch(error){
             });
             console.log("Fetch error:", error);
 }
-form.reset();
           }
 
 async function handleClick() {
   page += 1;
-  showLoadMoreButton();
+  hideLoadMoreButton(); 
+  showLoader();
   try {
      results = await getImagesByQuery(query, page);
+     hideLoader();
     createGallery(results.hits);
     const galleryItem = document.querySelector(".gallery-item");
     if (galleryItem) {
@@ -80,8 +83,15 @@ async function handleClick() {
         message: "We're sorry, but you've reached the end of search results.",
         position: 'topRight',
       });
-    }
+    }else {
+            showLoadMoreButton();
+        }
   } catch (error) {
+    hideLoader();
+    iziToast.error({
+            message: 'Error loading more images. Please try again.',
+            position: 'topRight',
+        });
     console.log("Помилка завантаження:", error);
   }
 }
